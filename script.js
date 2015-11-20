b972631a2fbc96ef204cdb97a3757110d57710ba
Grand Circus <alexvaughn121@gmail.com>
2015-11-20 14:24:05 -0500

    added assessment

---
+++ script.js
@@ -0,0 +1,21 @@
+(function(){var animals = [
+	{
+	  name:"cow", 
+	  sound:"moo"
+	},
+	{
+	  name:"sheep",
+	 sound:"baaah"
+	},
+	{
+	  name: "dog", 
+	  sound:"arf"
+	}];
+	function seeAndSay(animals){
+		seeAndSay.forEach(animals);
+	};
+	function(animals){
+		animals.forEach.push(color:grey);
+		console.log("The "+ animals.name + "says" + animals.sound + " ! And is " + animals.color);
+	};
+})();
